45810cbcef23a67486fc3ccee8833ab57c12c2ee
Devansh Sahu <sahu.devansh02@gmail.com>
2025-01-20 17:10:08 +0530

    Started Navigation

--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -1,18 +1,35 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+import { useWindow } from "../../utils/useWindow";
 
 function Orb(){
+
+    const {width, height} = useWindow()
+
+    console.log(width, height)
+
+    const moveOrb = keyframes`
+        0%{
+            transform: translate(0,0);
+            }
+        50%{
+             transform: translate(${width}px, ${height}px);
+             }
+        100%{
+             transform: translate(0,0);
+        }
+    `
 
     const OrbStyled = styled.div`
         width: 70vh;
         height: 70vh;
-        position: absolut;
+        position: absolute;
         border-radius: 50%;
         margin-left: -37vh;
         margin-top: -37vh;
         background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-        filter: blur(100px);
-
+        filter: blur(400px);
+        animation: ${moveOrb} 1s alternate linear infinite;
     `;
 
 
