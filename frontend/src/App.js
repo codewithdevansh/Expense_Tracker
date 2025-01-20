bd8d149c090631ad8bbad6898725718258a0b65a
Devansh Sahu <sahu.devansh02@gmail.com>
2025-01-20 15:31:04 +0530

    Initiliazed Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-
+import styled from "styled-components";
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <AppStyled bg={bg} className="App">
+      <Orb />
+     <MainLayout>
+       
+     </MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg})
+  position: relative;
+`
+
 
 export default App;
